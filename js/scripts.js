2b566023363fc6a9cfdd11258ff50753d352bfe3
seitakenaka <seitakenaka@gmail.com>
2020-08-06 12:11:09 +0200

    added pokemon data

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+var pokemonList = [
+  {
+    creatureName : 'bulbasaur',
+    creatureHeight : 0.7,
+    creatureWeight : 6.9,
+    creatureTypes : ['grass', 'poison'],
+    cratureAbilities : ['chlorophyll', 'overgrow'],
+  },
+  {
+    creatureName : 'charmander',
+    creatureHeight : 0.6,
+    creatureWeight : 8.5,
+    creatureTypes : ['fire'],
+    cratureAbilities : ['blaze', 'solar-power'],
+  },
+  {
+    creatureName : 'squirtle',
+    creatureHeight : 0.5,
+    creatureWeight : 9,
+    creatureTypes : ['water'],
+    cratureAbilities : ['rain-dish', 'torrent'],
+  },
+  {
+    creatureName : 'caterpie',
+    creatureHeight : 0.3,
+    creatureWeight : 2.9,
+    creatureTypes : ['bug'],
+    cratureAbilities : ['shield-dust', 'run-away'],
+  },
+  {
+    creatureName : 'weedle',
+    creatureHeight : 0.3,
+    creatureWeight : 3.2,
+    creatureTypes : ['bug', 'poison'],
+    cratureAbilities : ['shield-dust', 'run-away'],
+  }
+]
