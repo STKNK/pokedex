3190b60d4a35b4d602e61eacfa00c5a16210d7fd
seitakenaka <seitakenaka@gmail.com>
2020-11-10 18:14:13 +0100

    removed the parent function

--- js/scripts.js
+++ js/scripts.js
@@ -1,61 +1,58 @@
-// IIFE wrap
-(function () {
-  // create a new pokemonRepository variable
-  var pokemonRepository = (function () {
-    var pokemonList = [
-      {
-        creatureName : 'bulbasaur',
-        creatureHeight : 0.7,
-        creatureWeight : 6.9,
-        creatureTypes : ['grass', 'poison'],
-        cratureAbilities : ['chlorophyll', 'overgrow'],
-      },
-      {
-        creatureName : 'charmander',
-        creatureHeight : 0.6,
-        creatureWeight : 8.5,
-        creatureTypes : ['fire'],
-        cratureAbilities : ['blaze', 'solar-power'],
-      },
-      {
-        creatureName : 'squirtle',
-        creatureHeight : 0.5,
-        creatureWeight : 9,
-        creatureTypes : ['water'],
-        cratureAbilities : ['rain-dish', 'torrent'],
-      },
-      {
-        creatureName : 'caterpie',
-        creatureHeight : 0.3,
-        creatureWeight : 2.9,
-        creatureTypes : ['bug'],
-        cratureAbilities : ['shield-dust', 'run-away'],
-      },
-      {
-        creatureName : 'weedle',
-        creatureHeight : 0.3,
-        creatureWeight : 3.2,
-        creatureTypes : ['bug', 'poison'],
-        cratureAbilities : ['shield-dust', 'run-away'],
-      }
-    ];
-
-    //returns an array of Pokemon
-    function getAll(){
-      return pokemonList;
+// create a new pokemonRepository variable
+var pokemonRepository = (function () {
+  var pokemonList = [
+    {
+      creatureName : 'bulbasaur',
+      creatureHeight : 0.7,
+      creatureWeight : 6.9,
+      creatureTypes : ['grass', 'poison'],
+      cratureAbilities : ['chlorophyll', 'overgrow'],
+    },
+    {
+      creatureName : 'charmander',
+      creatureHeight : 0.6,
+      creatureWeight : 8.5,
+      creatureTypes : ['fire'],
+      cratureAbilities : ['blaze', 'solar-power'],
+    },
+    {
+      creatureName : 'squirtle',
+      creatureHeight : 0.5,
+      creatureWeight : 9,
+      creatureTypes : ['water'],
+      cratureAbilities : ['rain-dish', 'torrent'],
+    },
+    {
+      creatureName : 'caterpie',
+      creatureHeight : 0.3,
+      creatureWeight : 2.9,
+      creatureTypes : ['bug'],
+      cratureAbilities : ['shield-dust', 'run-away'],
+    },
+    {
+      creatureName : 'weedle',
+      creatureHeight : 0.3,
+      creatureWeight : 3.2,
+      creatureTypes : ['bug', 'poison'],
+      cratureAbilities : ['shield-dust', 'run-away'],
     }
+  ];
 
-    //add a single item to the pokemonList array
-    function add(item){
-      pokemonList.push(item);
-    }
+  //returns an array of Pokemon
+  function getAll(){
+    return pokemonList;
+  }
 
-    //return all functions
-    return {
-      getAll: getAll,
-      add: add
-    };
-  })();
+  //add a single item to the pokemonList array
+  function add(item){
+    pokemonList.push(item);
+  }
+
+  //return all functions
+  return {
+    getAll: getAll,
+    add: add
+  };
 })();
 
 document.write(pokemonRepository.getAll());
